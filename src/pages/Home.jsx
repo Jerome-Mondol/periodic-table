b574023c04be082e7da18540479b23c28172c54f
Jerome Mondol <jeromemondoll11@gmail.com>
2025-06-17 01:27:06 +0600

    initial landing page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Landing from "./Landing"
+
+const Home = () => {
+  return (
+    <>
+        <div className="h-screen w-screen background" >
+            <Landing />
+        </div>   
+    </>
+  )
+}
+
+export default Home
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,20 @@
+// Landing.jsx
+import '../index.css'
+import Table from '../assets/images/table.png' 
+
+const Landing = () => {
+  return (
+    <div className="background min-h-screen flex items-center pt-20 relative flex-col">
+      <h1 className="text-9xl text-[#4DA6FF] custom-font w-fit relative z-10 ">
+        Atomix
+      </h1>
+      <p className='custom-font text-2xl w-fit py-5 text-[#9D6BFF]' >Transform how you learn chemistry with our interactive periodic experience </p>
+
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Landing
